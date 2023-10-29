9ebcf032938fc57c53e2be2cd173a5e62b8e9c3f
maurobernal <info@maurobernal.com.ar>
2023-10-29 19:22:56 -0300

    feat:MB: StartUp

---
+++ app/ui/font.ts
@@ -0,0 +1,4 @@
+import { Inter } from "next/font/google";
+import { Lusitana } from "next/font/google";
+export const fontInter = Inter({ subsets: ["latin"] });
+export const fontLusitana = Lusitana({ weight: "400", subsets: ["latin"] });
